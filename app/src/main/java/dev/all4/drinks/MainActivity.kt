fb7a7a731cf566287f6ff806e90813b5a630a7ff
Livio Lopez <liviolopez@gmail.com>
2020-11-10 19:59:13 -0500

    implemented all method, classes, xml and gradle files of and Android App

---
+++ app/src/main/java/dev/all4/drinks/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.all4.drinks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.all4.all4dev.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath(kotlin("gradle-plugin", version = "1.4.10"))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}

2169f8aad19d848f1205e700942e9cf0aa92e9a9
Livio Lopez <liviolopez@gmail.com>
2020-11-10 20:15:03 -0500

    cleanup unused method

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,6 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
-//    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-//    id("org.jetbrains.kotlin.jvm") version "1.4.10"
-//
-//    // Apply the application plugin to add support for building a CLI application in Java.
-//    application
-
     id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
@@ -41,22 +35,9 @@ android {
     buildFeatures.dataBinding = true
 }
 
-repositories {
-    // Use JCenter for resolving dependencies.
-    jcenter()
-}
-
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:29.0-jre")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
--- app/src/main/kotlin/dev/all4/drinks/App.kt
+++
@@ -1,7 +0,0 @@
-package dev.all4.drinks
-
-import android.app.Application
-
-class App: Application() {
-
-}

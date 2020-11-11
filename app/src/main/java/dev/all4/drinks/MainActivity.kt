e256b2f3619ab6dfe30bd402aeafb71b9d915c06
Livio Lopez <liviolopez@gmail.com>
2020-11-11 00:10:04 -0500

    basic navigation with component between two fragments

--- app/src/main/java/dev/all4/drinks/MainActivity.kt
+++ app/src/main/java/dev/all4/drinks/MainActivity.kt
@@ -2,11 +2,25 @@ package dev.all4.drinks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import dev.all4.all4dev.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        //set the actionBar with the navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
---
+++ app/src/main/java/dev/all4/drinks/ui/MainFragment.kt
@@ -0,0 +1,32 @@
+package dev.all4.drinks.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dev.all4.all4dev.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_cocktail.setOnClickListener {
+            findNavController().navigate(R.id.cocktailFragment)
+        }
+    }
+}
